e1d74e878b86193b3593696eefefc82775d21193
ZLaptop <zlaptop@ZLaptop>
2017-10-18 23:46:24 +0200

    Waves

--- src/santiagoCorrea_tema1b/Equacion2.java
+++ src/santiagoCorrea_tema1b/Equacion2.java
@@ -1,66 +1,55 @@
 package santiagoCorrea_tema1b;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-class RepeatEquPane extends JPanel {
-	double[] numbers;
+class Waves extends JPanel {
 
-	int endPoint = 0;
+	private int factor = 200;
+	private int points;
+	private double[] sines;
+	private int[] pts;
 
-	public RepeatEquPane(double[] numbers) {
-		this.numbers = numbers;
+	public Waves(int repeticiones) {
+		points = factor * repeticiones * 2;
+		sines = new double[points];
+		for (int i = 0; i < points; i++) {
+			double radians = (Math.PI / factor) * i;
+			sines[i] = Math.sin(radians);
+		}
 	}
 
 	@Override
 	public void paintComponent(Graphics g) {
-		Utils.drawAxis(g, this);
-		this.drawEquacionXTimes(g, numbers[0], numbers[1], numbers[2], -50, 50, 3);
-	}
-
-	private void drawEquacionXTimes(Graphics g, double a, double b, double c, double minX, double maxX, int times) {
-		g.setColor(Utils.getRandomColor());
-
-		for (int x = 0; x < times; x++) {
-
-			for (double i = minX + (endPoint * x); i < maxX + (endPoint * x); i++) {
-				/*
-				 * Formula para sacar los puntos de union entre las lineas
-				 */
-				double y = ((double) Math.pow(i, 2) * a) + i * b + c;
-				double xp = i + 1;
-				double yp = ((double) Math.pow(xp, 2) * a) + xp * b + c;
-				/*
-				 * Dibujar las lineas que forman la parabola
-				 */
-				if (x == 0 && i == maxX - 1) {
-					endPoint = (int) i;
-				}
-				g.drawLine((int) (realX(i)), (int) (realY(y)), (int) (realX(xp)), (int) (realY(yp)));
-			}
+		super.paintComponent(g);
+		int maxWidth = getWidth();
+		double hstep = (double) maxWidth / (double) points;
+		int maxHeight = getHeight();
+		pts = new int[points];
+		for (int i = 0; i < points; i++) {
+			pts[i] = (int) (sines[i] * maxHeight / 2 * .95 + maxHeight / 2);
 		}
-	}
-
-	/*
-	 * Cordenadas reales parser
-	 */
-	private double realX(double x) {
-		return (x + this.getWidth() / 2);
-	}
-
-	private double realY(double y) {
-		return (-y + this.getWidth() / 2);
+		g.setColor(Color.RED);
+		for (int i = 1; i < points; i++) {
+			int x1 = (int) ((i - 1) * hstep);
+			int x2 = (int) (i * hstep);
+			int y1 = pts[i - 1];
+			int y2 = pts[i];
+			g.drawLine(x1, y1, x2, y2);
+		}
 	}
 
 }
 
 public class Equacion2 extends JFrame {
+
 	public Equacion2() {
 		super("Equacion 2");
-		super.add(new RepeatEquPane(new double[] { -0.07, -8, -5 }));
-
+		Waves wave = new Waves(5);
+		add(wave);
 		init();
 	}
 
