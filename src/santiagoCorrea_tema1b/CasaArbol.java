deddfe9ca073ac2989ec0a2fa87b04a8f7992ac3
Santi <santi@Mac-Pro-de-Jordi-Girones.local>
2017-10-20 21:07:34 +0200

    CasaArbol terminada

--- src/santiagoCorrea_tema1b/CasaArbol.java
+++ src/santiagoCorrea_tema1b/CasaArbol.java
@@ -11,6 +11,7 @@ class MainPane extends JPanel {
 	public void paintComponent(Graphics g) {
 		drawSol(g);
 		drawTree(g);
+		drawHouse(g);
 	}
 
 	private void drawSol(Graphics g) {
@@ -32,21 +33,72 @@ class MainPane extends JPanel {
 		g.drawLine(getWidth() - 150, 90, getWidth() - 220, 120);
 
 	}
-	
+
 	private void drawTree(Graphics g) {
-		//TREE
+		// TREE
 		g.setColor(Color.BLACK);
 		g.drawLine(getWidth() - 120, getHeight() - 50, getWidth() - 120, getHeight() - 200);
 		g.drawLine(getWidth() - 140, getHeight() - 50, getWidth() - 140, getHeight() - 200);
 		g.drawLine(getWidth() - 140, getHeight() - 200, getWidth() - 120, getHeight() - 200);
-		
+
 		// LEAVES
 		g.setColor(Color.GREEN);
 		g.drawOval(getWidth() - 120, getHeight() - 250, 70, 70);
 		g.drawOval(getWidth() - 180, getHeight() - 300, 100, 100);
 		g.drawOval(getWidth() - 170, getHeight() - 230, 60, 60);
 		g.drawOval(getWidth() - 230, getHeight() - 280, 100, 100);
+	}
 
+	private void drawHouse(Graphics g) {
+		// HOUSE
+		g.setColor(Color.BLACK);
+		g.drawLine(100, getHeight() - 50, 100, getHeight() - 200);
+		g.drawLine(250, getHeight() - 50, 250, getHeight() - 200);
+
+		// DOOR
+		g.drawLine(150, getHeight() - 50, 150, getHeight() - 125);
+		g.drawLine(200, getHeight() - 50, 200, getHeight() - 125);
+		g.drawLine(200, getHeight() - 125, 150, getHeight() - 125);
+
+		g.drawLine(150, getHeight() - 125, 190, getHeight() - 105);
+		g.drawLine(150, getHeight() - 50, 190, getHeight() - 30);
+		g.drawLine(190, getHeight() - 30, 190, getHeight() - 105);
+
+		g.drawOval(180, getHeight() - 70, 10, 10);
+
+		// WINDOWS
+		g.drawRect(120, getHeight() - 190, 40, 40);
+		g.drawRect(190, getHeight() - 190, 40, 40);
+		g.drawLine(140, getHeight() - 190, 140, getHeight() - 150);
+		g.drawLine(210, getHeight() - 190, 210, getHeight() - 150);
+
+		// ROOF
+		g.setColor(Color.RED);
+		g.drawLine(60, getHeight() - 200, 100, getHeight() - 200);
+		g.drawLine(60, getHeight() - 200, 175, getHeight() - 275);
+
+		g.drawLine(290, getHeight() - 200, 175, getHeight() - 275);
+		g.drawLine(290, getHeight() - 200, 250, getHeight() - 200);
+
+		g.drawLine(260, getHeight() - 220, 260, getHeight() - 275);
+		g.drawLine(230, getHeight() - 240, 230, getHeight() - 275);
+		g.drawLine(230, getHeight() - 275, 260, getHeight() - 275);
+
+		g.setColor(getBackground());
+		g.fillRect(230, getHeight() - 239, 30, 30);
+
+		// FLOOR
+		g.setColor(Color.GREEN);
+		g.drawLine(0, getHeight() - 50, 150, getHeight() - 50);
+		g.drawLine(200, getHeight() - 50, getWidth() - 140, getHeight() - 50);
+		g.drawLine(getWidth() - 120, getHeight() - 50, getWidth(), getHeight() - 50);
+		
+		//SMOKE
+		g.setColor(Color.GRAY);
+		g.drawOval(240, getHeight() - 300, 20, 20);
+		g.drawOval(220, getHeight() - 340, 40, 40);
+		g.drawOval(260, getHeight() - 380, 40, 50);
+		g.drawOval(200, getHeight() - 420, 60, 50);
 
 	}
 }
@@ -62,7 +114,7 @@ public class CasaArbol extends JFrame {
 
 	private void init() {
 		super.setResizable(false);
-		super.setSize(500, 500);
+		super.setSize(600, 500);
 		super.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		super.setVisible(true);
 	}
